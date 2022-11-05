45d9615a56b6b406f6e433aa2db963d638e74e1a
b2p <>
2022-11-05 01:22:27 -0700

    [BE] add genChainId.js

---
+++ backend/src/chainId/genChainId.js
@@ -0,0 +1,7 @@
+export async function genChainId() {
+  // TODOs:
+  // - generate a random chain id
+  // - avoid chain ids at chainlist.org
+  // - avoid choosing a chainId in the local redis database
+  return 1234321;
+}
