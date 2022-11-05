dc10467e4274e9ca288cb31d71d54fc08568e710
b2p <>
2022-11-05 10:59:18 -0700

    [BE] add env var IS_APPLE_CHIP

--- backend/src/genesis/initGenesisBlock.js
+++ backend/src/genesis/initGenesisBlock.js
@@ -1,10 +1,13 @@
 import { execAsync } from '../utils/execAsync.js';
 
 export async function initGenesisBlock(file, datadir) {
-  // TODO:
-  // add "--platform linux/amd64" for Apple chips
-  const cmd =
-    `docker run -d --rm -v ${datadir}:${datadir} ` +
+  // TODO: extract this code snippet to a module
+  let cmd = 'docker run -d --rm ';
+  if (process.env.IS_APPLE_CHIP === 'true') {
+    cmd += '--platform linux/amd64 ';
+  }
+  cmd +=
+    `-v ${datadir}:${datadir} ` +
     'ethereum/client-go:alltools-latest ' +
     `geth init --datadir ${datadir} ${file}`;
 
