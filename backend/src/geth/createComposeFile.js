1c92995a40bea5073c156f59a12f478c32237dc2
b2p <>
2022-11-05 17:01:02 -0700

    [BE] add more logic in createComposeFile.js

--- backend/src/geth/createComposeFile.js
+++ backend/src/geth/createComposeFile.js
@@ -1,6 +1,23 @@
+import yaml from 'js-yaml';
+import fs from 'fs';
+import path from 'path';
+import { dirName } from '../utils/dirName.js';
+
 export async function createComposeFile(dataDir, nodekey, port, networkId) {
+  // - become a bootnode itself
   // TODO: this should be another env var instead of hardcode to always be addrs 1
   const unlock = '0x' + process.env.SEALER_ADDRESS_1;
   // TODO:
   // - password from env file
+  try {
+    const content = fs.readFileSync(
+      path.resolve(dirName(import.meta.url), process.env.COMPOSE_TEMPLATE)
+    );
+    const doc = yaml.load(content);
+    console.log(doc.services.geth.command);
+    console.log(doc.services.geth.ports);
+    console.log(doc.services.geth.volumes);
+  } catch (e) {
+    console.log(e);
+  }
 }
