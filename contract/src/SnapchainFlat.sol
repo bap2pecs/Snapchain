887ddecb80db3f43a14c3d0a27c8d85e21db5b8f
0xMojo7 <mojo@malt.money>
2022-11-05 20:31:23 -0700

    add split sig func for frontend helper

--- contract/src/SnapchainFlat.sol
+++ contract/src/SnapchainFlat.sol
@@ -1532,5 +1532,15 @@ contract Snapchain {
 
         emit SecondPriceChanged(_secondPrice);
     }
+
+    // SIGNATURE HELPER
+    function splitSignature(bytes memory sig) public pure returns (bytes32 r, bytes32 s, uint8 v){
+        require(sig.length == 65, "invalid signature length");
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
 }
 
